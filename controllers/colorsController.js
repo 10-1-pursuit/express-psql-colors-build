8e738ea97a963529ce3c4ece201b6e91602a568b
Carlos Godoy <carlosgodoy@pursuit.org>
2023-10-31 20:43:51 -0400

    updating seed & read

--- controllers/colorsController.js
+++ controllers/colorsController.js
@@ -3,8 +3,13 @@ const colors = express.Router()
 
 
 // GET all colors
-colors.get("/", (req, res) => {
-    res.json( { status: "ok" } )
+colors.get("/", async (req, res) => {
+    const allColors = await getAllColors()
+    if(allColors[0]){
+        res.status(200).json(allColors)
+    } else {
+        res.status(500).json({ error: "server error" })
+    }
 })
 
 
