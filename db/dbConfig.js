263a28ac9f6684fa29c902a16c29b19786c01274
Carlos Godoy <carlosgodoy@pursuit.org>
2023-11-01 23:29:45 -0400

    removing password from cn

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -6,8 +6,7 @@ const cn = {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD
+    user: process.env.PG_USER
 }
 
 // Connect to colors_dev db
